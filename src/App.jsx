3a28de67def0152f76a84e3a28cf57455f05e017
tontpong <tontpong@users.noreply.github.com>
2024-07-09 08:41:15 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import ArtifactCode from "./ArtifactCode";
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 function App() {
   return (
     <>
+      <SpeedInsights/>
       <ArtifactCode />
     </>
   );
