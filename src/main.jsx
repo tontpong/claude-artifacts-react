bf2badb9c3e59b362aa43ab541530e80dbadd184
tontpong <tontpong@users.noreply.github.com>
2024-07-09 08:30:31 +0700

    Update main.jsx .. speed insight

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <SpeedInsights/>
   <React.StrictMode>
     <App />
   </React.StrictMode>,
